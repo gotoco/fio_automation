7ceb2d42fc3c14cc8d3672393bff02488d4d0a5b
Maciej Grochowski <Maciej.Grochowski89@gmail.com>
2018-04-02 13:28:53 +0100

    #: Allow parameter input to be an output of python script value

--- configuration.py
+++ configuration.py
@@ -1,4 +1,5 @@
 import configparser
+from importlib import import_module
 
 
 def extract_man_field(config, section, fname):
@@ -8,6 +9,16 @@ def extract_man_field(config, section, fname):
         print('Error during configuration parsing!\nMandatory field: {} in section {} missing:\n{}'
               .format(section, fname, ex))
         return
+    # We treat all inputs with pattern {$XXX.YY} 
+    # notice some of FIO variable can be used as a $XXX so we allow them
+    # value is a python script
+    if val.startswith('${') and val.endswith('}') and '.' in val:
+        name = val.strip('$').strip('{}')
+        p, m = name.rsplit('.', 1)
+
+        mod = import_module(p)
+        met = getattr(mod, m)
+        return met()
 
     return val
 
